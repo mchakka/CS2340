eb294d2060571bfafaf785f93040b8dd46883e94
jaradhosking <jarad.hosking@gmail.com>
2018-09-07 15:02:47 -0400

    added person6 functionality to AntLabMain.java

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -75,23 +75,27 @@ import edu.gatech.oad.antlab.pkg2.*;
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("P5");
                   toPrint += p5.toString("gburdell5");
-		  
+                  //Person5 replace P4 with your name
+                  //and gburdell5 with your gt id
+                  Person5 p6 = new Person6("P6");
+                  toPrint += p6.toString("gburdell5");
+
         System.out.println(toPrint);
-        
-        
+
+
     }
-     
-    
+
+
     /**
      * entry point for the program
      */
      public static void main(String[] args) {
-        
+
         new AntLabMain().printOutMessage();
-        
-     } 
-    
-    
-    
-    
- } 
+
+     }
+
+
+
+
+ }
